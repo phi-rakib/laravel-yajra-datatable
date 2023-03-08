fc0985bd81b219f999aba0518e6d6e06dec90e31
rakib <phi.rakib@gmail.com>
2023-03-08 11:44:07 +0600

    fixed empty form submission on create new user

--- resources/views/users/modal.blade.php
+++ resources/views/users/modal.blade.php
@@ -29,7 +29,7 @@
 
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" id="saveData">Save</button>
+                        <input type="submit" class="btn btn-primary" id="saveData" value="Save">
                     </div>
                 </form>
             </div>
